7d8c39f1b5b6debe23f4ba1348d4917564f21091
brikez <brike@DESKTOP-F74V8DB>
2023-08-23 19:50:03 +0300

    replace Credit and Debt to one Entity Contract

---
+++ PersonFinance.DAL.API/PersonFinanceContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonFinance.API.Domain.Entities;
+using System.Reflection;
+
+namespace PersonFinance.API.DAL
+{
+    partial class PersonFinanceContext : DbContext
+    {
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+
+        public PersonFinanceContext(DbContextOptions<PersonFinanceContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PersonFinance.Domain.API/Entities/Contract.cs
@@ -0,0 +1,22 @@
+﻿using PersonFinance.API.Domain.Entities.structs;
+
+namespace PersonFinance.API.Domain.Entities
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+        public string Person { get; set; }
+        public DateTimeOffset ReceiptDate { get; set; }
+        public decimal InterestRate { get; set; }
+        public Money MoneyCredit { get; set; }
+        public bool Returned { get; set; }
+        public DateTimeOffset ReturnedDate { get; set; }
+        public Money ReturnedMoney { get; set; }
+        public TypeContract TypeContract { get; set; }
+    }
+    public enum TypeContract
+    {
+        Credit,
+        Debt,
+    }
+}
