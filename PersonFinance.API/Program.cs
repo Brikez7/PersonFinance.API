a2034e5d5c3ed51b1f0b7e1c93ce1730aba58bcc
brikez <brike@DESKTOP-F74V8DB>
2023-08-23 16:04:03 +0300

    Architecture Initialization

--- PersonFinance.API/Program.cs
+++ PersonFinance.API/Program.cs
@@ -7,16 +7,12 @@ namespace PersonFinance.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -24,10 +20,7 @@ namespace PersonFinance.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
