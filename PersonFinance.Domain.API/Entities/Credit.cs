059ed2cd57c3e20455f4ca407a26c2dd8df86ec4
brikez <brike@DESKTOP-F74V8DB>
2023-08-23 19:05:13 +0300

    Create entities

---
+++ PersonFinance.Domain.API/Entities/Credit.cs
@@ -0,0 +1,26 @@
+﻿using PersonFinance.API.Domain.Entities.structs;
+
+namespace PersonFinance.API.Domain.Entities
+{
+    public class Credit
+    {
+        public int Id { get; set; }
+        public Money MoneyCredit { get; set; }
+        public decimal InterestRate { get; set; }
+        public bool Returned { get; set; }
+        public DateTimeOffset ReturnedDate { get; set; }
+        public Money ReturnedMoney { get; set; }
+        public string Debtor { get; set; }
+
+        public Credit(int id, Money moneyCredit, decimal interestRate, bool returned, DateTimeOffset returnedDate, Money returnedMoney, string debtor)
+        {
+            Id = id;
+            MoneyCredit = moneyCredit;
+            InterestRate = interestRate;
+            Returned = returned;
+            ReturnedDate = returnedDate;
+            ReturnedMoney = returnedMoney;
+            Debtor = debtor;
+        }
+    }
+}
---
+++ PersonFinance.Domain.API/Entities/Income.cs
@@ -0,0 +1,20 @@
+﻿using PersonFinance.API.Domain.Entities.structs;
+
+namespace PersonFinance.API.Domain.Entities
+{
+    public class Income
+    {
+        public Guid Id { get; set; }
+        public Money MoneyReceived { get; set; }
+        public DateTimeOffset ReceiptDate { get; set; }
+        public string TypeActivity { get; set; }
+
+        public Income(Guid id, Money moneyReceived, DateTimeOffset receiptDate, string typeActivity)
+        {
+            Id = id;
+            MoneyReceived = moneyReceived;
+            ReceiptDate = receiptDate;
+            TypeActivity = typeActivity;
+        }
+    }
+}
