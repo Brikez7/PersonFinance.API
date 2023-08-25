2384da78ffdc105ea5c6cb1d47cda274187e0fa2
brikez <brike@DESKTOP-F74V8DB>
2023-08-25 14:56:30 +0300

    Implementation IncomeController.

---
+++ PersonFinance.API/Controllers/IncomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonFinance.API.DAL.Repositories;
+using PersonFinance.API.Domain.Entities;
+
+namespace PersonFinance.API.Controllers
+{
+    public class IncomeController : ControllerBase
+    {
+        private readonly IGenericRepository<Income> _IncomeRepository;
+        private readonly ILogger<IncomeController> _logger;
+        public IncomeController(IGenericRepository<Income> moneyAccountRepository, ILogger<IncomeController> logger)
+        {
+            _IncomeRepository = moneyAccountRepository;
+            _logger = logger;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var incomes = await _IncomeRepository.Get().ToArrayAsync();
+            return Ok(incomes);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] Income income)
+        {
+            var newIncomes = await _IncomeRepository.AddAsync(income);
+            await _IncomeRepository.SaveChangesAsync();
+            return Ok(new Tuple<bool, Income?>(true, newIncomes));
+        }
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var Income = await _IncomeRepository.Get().FirstOrDefaultAsync(x => x.Id == id);
+            if (Income is not null)
+            {
+                _IncomeRepository.Remove(Income);
+                await _IncomeRepository.SaveChangesAsync();
+                return Ok(true);
+            }
+
+            return Ok(false);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Put([FromBody] Income income)
+        {
+            var finedIncome = await _IncomeRepository.Get().AnyAsync(x => x.Id == income.Id);
+            if (finedIncome)
+            {
+                _IncomeRepository.Update(income);
+                await _IncomeRepository.SaveChangesAsync();
+                return Ok(new Tuple<bool, Income?>(true, income));
+            }
+            return Ok(new Tuple<bool, Income?>(false, income));
+        }
+    }
+}
